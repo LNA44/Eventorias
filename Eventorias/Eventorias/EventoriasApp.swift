351fe2c3f513dc77ccc1376cb70cae966cac1b4e
Elena <elenabougaran@gmail.com>
2025-09-30 16:15:05 +0200

    Ajout du fichier .gitignore et création du projet dans Xcode

---
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Ordinateur elena on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
